e32d56cd1ba69821ae40f4252e1a5a6242684289
Noah Ham <noahham25@gmail.com>
2025-05-06 11:52:54 -0700

    updated variable calls

--- character-split/split.jsx
+++ character-split/split.jsx
@@ -2,51 +2,51 @@
 (function () {
     app.beginUndoGroup("Split Text to Characters");
 
-    var comp = app.project.activeItem;
+    let comp = app.project.activeItem;
     if (!(comp && comp instanceof CompItem)) {
         alert("Please select a composition.");
         return;
     }
 
-    var selectedLayers = comp.selectedLayers;
+    let selectedLayers = comp.selectedLayers;
     if (selectedLayers.length === 0 || !(selectedLayers[0] instanceof TextLayer)) {
         alert("Please select a text layer.");
         return;
     }
 
-    var originalLayer = selectedLayers[0];
-    var textDocument = originalLayer.text.sourceText.value;
-    var numChars = textDocument.text.length;
+    let originalLayer = selectedLayers[0];
+    let textDocument = originalLayer.text.sourceText.value;
+    let numChars = textDocument.text.length;
 
-    var baseTextProp = originalLayer.text.sourceText.value;
+    let baseTextProp = originalLayer.text.sourceText.value;
 
     // Create a temporary text layer to measure character widths
-    var tempLayer = comp.layers.addText("W");
+    let tempLayer = comp.layers.addText("W");
     tempLayer.enabled = false;
     tempLayer.moveBefore(originalLayer);
 
-    var charX = 0;
-    for (var i = 0; i < numChars; i++) {
-        var char = textDocument.text[i];
+    let charX = 0;
+    for (let i = 0; i < numChars; i++) {
+        let char = textDocument.text[i];
 
         // Skip invisible characters (like space, newline)
         if (char === "\r" || char === "\n") continue;
 
-        var newLayer = originalLayer.duplicate();
+        let newLayer = originalLayer.duplicate();
         newLayer.name = "Char_" + char + "_" + (i + 1);
 
         // Set only the current character as the text
-        var newTextDoc = newLayer.text.sourceText.value;
+        let newTextDoc = newLayer.text.sourceText.value;
         newTextDoc.text = char;
         newLayer.text.sourceText.setValue(newTextDoc);
 
         // Use tempLayer to measure each character width up to current
         tempLayer.text.sourceText.setValue(baseTextProp);
         tempLayer.text.sourceText.setValue(baseTextProp.text.substring(0, i));
-        var offset = tempLayer.sourceRectAtTime(comp.time, false).width;
+        let offset = tempLayer.sourceRectAtTime(comp.time, false).width;
 
         // Set the position based on offset
-        var originalPos = originalLayer.property("Position").value;
+        let originalPos = originalLayer.property("Position").value;
         newLayer.property("Position").setValue([
             originalPos[0] + offset,
             originalPos[1]
