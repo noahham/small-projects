c4c1b09c6af1d2f1632a064e8742f864da992e40
Noah <68747182+noahham@users.noreply.github.com>
2025-05-18 01:20:39 -0700

    Update repo-flash.py

--- repo-flashbang/repo-flash.py
+++ repo-flashbang/repo-flash.py
@@ -1,1 +1,33 @@
-asd
+import time
+from pywinauto import Application, findwindows
+from pynput.keyboard import Controller, Key
+
+keyboard = Controller()
+
+def send_repo_chat_message(message: str, window_title_regex: str = r".*R\.E\.P\.O.*"):
+    try:
+        app = Application().connect(title_re=window_title_regex)
+    except findwindows.ElementNotFoundError:
+        print("Could not find a window matching", window_title_regex)
+        return
+
+    repo_win = app.window(title_re=window_title_regex)
+    repo_win.set_focus()
+    time.sleep(0.1)
+
+    # Press T to open chat
+    keyboard.press('t')
+    keyboard.release('t')
+    time.sleep(0.1)
+
+    # Type message
+    keyboard.type(message)
+    time.sleep(0.1)
+
+    # Press Enter to send
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+
+if __name__ == "__main__":
+    chat_message = "<size=-11111><mark=#ffffff>FLASH"
+    send_repo_chat_message(chat_message)
